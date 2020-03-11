4ed8c0719adb9c80207c3e625115f9c235747052
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-03-11 16:17:48 +0000

    merge police and postcode api Relate #3
    Co-authored-by: chloe <53328450+Chloeh24@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const postcode = document.querySelector(".postcode")
+const postcode = document.querySelector(".postcode");
+let lat = 0;
+let long = 0;
 
-postcode.addEventListener('submit', event => {
-    event.preventDefault();
-    const postcodeValue = event.target.elements.postcode.value
-    fetch(`https://api.postcodes.io/postcodes/${postcodeValue}`)
-    .then(response => {
-        if (!response.ok) throw new Error(response.status)
-        return response.json()
-    })
+postcode.addEventListener("submit", event => {
+  event.preventDefault();
+  const postcodeValue = event.target.elements.postcode.value;
+  fetch(`https://api.postcodes.io/postcodes/${postcodeValue}`)
+    .then(dealWithResponse)
     .then(data => {
-        var lat = 0;
-        lat += data.result.latitude
-        var long = 0;
-        long += data.result.longitude
+      lat = data.result.latitude;
+      long = data.result.longitude;
+    //   console.log(lat, long);
+      return fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${long}`);
     })
-})
+    .then(dealWithResponse)
+    .then(data => {
+      data.forEach(item => console.log(item.category));
+      // console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+function dealWithResponse(response) {
+    if (!response.ok) throw new Error(response.status);
+      return response.json();
+}
