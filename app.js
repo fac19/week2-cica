ea4635d4096722efcc44d1322fe0f36813e416f6
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-03-12 14:55:08 +0000

    write a funtion that automatically creates svgs

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ let long = -0.116773;
 let zoom = 8
 let totalCrimes = 0
 let crimes = {}
-
+let postcodeValue = ""
 let date = document.querySelector("[name=date]")
 
+const svg = document.querySelector("svg")
+console.log(svg)
+const svgtitle = document.querySelector("#svgTitle")
 
 postcode.addEventListener("submit", event => {
   event.preventDefault();
-  const postcodeValue = event.target.elements.postcode.value;
+   postcodeValue = event.target.elements.postcode.value;
   fetch(`https://api.postcodes.io/postcodes/${postcodeValue}`)
     .then(dealWithResponse)
     .then(data => {
@@ -35,6 +38,7 @@ postcode.addEventListener("submit", event => {
         console.log(totalCrimes, crimes)
     })
     .then(initMap)
+    .then(createSvgContent)
     .catch(error => {
       console.log(error);
     });
@@ -52,4 +56,29 @@ function dealWithResponse(response) {
     var map = new google.maps.Map(
       document.getElementById('map'), {zoom: zoom, center: yourArea});
       var marker = new google.maps.Marker({position: yourArea, map: map});
+  }
+
+  function createSvgContent() {
+    var svgNS = "http://www.w3.org/2000/svg"; 
+    svgtitle.innerText = `Crime Statistics in ${postcodeValue}`
+    const title = document.createElement("title")
+    title.innerText = `Crime Statistics in ${postcodeValue}`
+    title.setAttribute("id", "title")
+    svg.appendChild(title)
+    
+    const g = document.createElementNS(svgNS, "g")
+    g.classList.add("bar")
+    svg.appendChild(g)
+
+    const rect = document.createElement("rect")
+    rect.setAttribute("width", crimes.burglary)
+    rect.setAttribute("height", "20")
+    g.appendChild(rect)
+
+    const text = document.createElement("text")
+    text.setAttribute("x", "45")
+    text.setAttribute("y", "9.5")
+    text.setAttribute("dy", "0.35em")
+    text.innerText = "fake name"
+    g.appendChild(text)
   }
